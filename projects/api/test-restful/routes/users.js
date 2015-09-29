bbf8b398aa8dda6fcc74c3dea9c2a69e29030d5a
nanouwaiwai@icloud.com <nanouwaiwai@icloud.com>
2015-09-29 11:13:49 +0800

    add user data

--- projects/api/test-restful/routes/users.js
+++ projects/api/test-restful/routes/users.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    var users = {
+        "users": [
+            {
+                "name": "张三",
+                "sex": "男",
+                "age": "27"
+            },
+            {
+                "name": "李四",
+                "sex": "男",
+                "age": "26"
+            },
+            {
+                "name": "王红",
+                "sex": "女",
+                "age": "23"
+            }]
+    };
+    res.send('respond with a resource');
 });
 
 module.exports = router;
